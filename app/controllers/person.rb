3491b0dcf32f31f9a1ad8396e6ae36d1027455f0
Joseph Dickinson <Joe@unknown-b8:e8:56:14:c6:a8.home>
2013-10-26 15:40:56 +0100

    lastest version

--- app/controllers/person.rb
+++ app/controllers/person.rb
@@ -1,6 +1,79 @@
 PadrinoAddressBook::App.controllers :person do
   
-  # get :index, :map => '/foo/bar' do
+
+  layout :main
+
+  before do
+    #binding.pry
+    redirect '/person/login' unless session[:logged_in] || request.fullpath == '/person/login'
+    # env["PATH_INFO"] == '/person/login'
+  end
+
+  get :login do
+    render 'person/login', :layout => false
+  end
+
+  post :login do
+    person = Person.find_by_email(params[:email])
+      # binding.pry
+    if person && person.password == params[:password]
+      session[:logged_in] = true
+      redirect '/person'
+    else
+      flash_tag(:notice, :id => 'Record not found.')
+      flash[:notice] = "Email and password do not match."
+      render 'person/login', :layout => false
+    end
+  end
+
+  get :index do
+    @people = Person.all
+    render 'person/index'
+  end
+
+  post :create, {:map => '/person/new'} do
+    p = Person.new(params["person"])
+    p.save
+    flash_tag(:notice, :id => 'Entry Created!')
+    flash[:notice] = "Entry Created!"
+    redirect '/person/new'
+  end
+
+  get :edit, :with => :id, :map => '/person/edit' do
+    @person = Person.find(params[:id])
+    render 'person/edit'
+  end
+
+  put :update, :map => '/person' do
+    binding.pry
+    p = Person.find(params[:person][:_id])
+    p.update_attributes(params[:person])
+    binding.pry
+    flash_tag(:notice, :id => 'Updated!')
+    flash[:notice] = "Saved!"
+    # redirect 'person/index'
+    # /edit/#{p._id}
+    # render 'person/index'
+  end
+
+  get :search, :with => :letter, :map => '/person/find' do
+    @people = Person.find_by_letter(params[:letter])
+    render 'person/index'
+  end
+
+  get :new, :map => '/person/new' do
+    @person = Person.new
+    render 'person/new'
+  end
+
+  # destroy :edit, :with => :id, :map => '/person' do
+  #   @person = Person.find(params[:id])
+    # @person.destroy
+  # end
+
+
+
+    # get :index, :map => '/foo/bar' do
   #   session[:foo] = 'bar'
   #   render 'index'
   # end
@@ -19,74 +92,5 @@ PadrinoAddressBook::App.controllers :person do
   #   'Hello world!'
   # end
   
-  # All routes on this controller use the main layout
-  # enable :authentication
-  # set :login_page, '/person/login'
-
-  # enable :authentication
-  # enable :store_location
-  # set    :login_page, "/person/login"
-
-  layout :main
-
-  before do
-    binding.pry
-    redirect '/person/login' unless session[:logged_in] || request.fullpath == '/person/login'
-    # env["PATH_INFO"] == '/person/login'
-  end
-
-  get '/login' do
-    render 'person/login', :layout => false
-  end
-
-  post 'login' do
-    person = Person.find_by_email(params[:email])
-       # binding.pry
-    if person && person.password == params[:password]
-      session[:logged_in] = true
-      redirect '/person/index'
-    else
-      flash_tag(:notice, :id => 'Record not found.')
-      flash[:notice] = "Email and password do not match."
-      render 'person/login', :layout => false
-    end
-  end
-
-  get '/index' do
-    @people = Person.all
-    render 'person/index'
-  end
-
-  get '/new' do
-    #NOT erb :index!
-    @person = Person.new
-    render '/person/new' 
-    #, :layout => 'main'
-  end
-
-  post '/create' do
-    #binding.pry
-    p = Person.new(params["person"])
-    p.save
-    flash[:notice] = "Entry Created!"
-    redirect '/person/new'
-  end
-
-  get '/edit', :with => :id do
-    @person = Person.find(params[:id])
-    render 'person/edit'
-  end
-
-  post '/update' do
-    p = Person.find(params[:person][:id])
-    p.update_attributes(params[:person])
-    flash[:notice] = "Saved!"
-    redirect '/person/#{p._id}'
-  end
-
-  get '/search', :with => :letter do
-    @people = Person.find_by_letter(params[:letter])
-    render 'person/index'
-  end
 
 end
